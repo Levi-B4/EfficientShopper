a0c583003d52abb875881c35a1b3ebfce4dbaf45
Levi-B4 <levi.butler4@gmail.com>
2023-07-26 18:26:42 -0400

    Updated product price to be optional, added migration, and updated ef database

---
+++ ShoppingListAPI/Migrations/20230726192156_RemovePriceRequirement.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingListAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePriceRequirement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "price",
+                table: "Products",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "price",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- ShoppingListAPI/Models/Product.cs
+++ ShoppingListAPI/Models/Product.cs
@@ -1,11 +1,17 @@
-﻿namespace ShoppingListAPI.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingListAPI.Models
 {
     public class Product
     {
         public Guid ID { get; set; }
+        [MaxLength(30)]
         public string name { get; set; }
+        [Comment("Short description to clarify product if wanted")]
         public string? description { get; set; }
-        public decimal price { get; set; }
+        public decimal? price { get; set; }
         public decimal quantity { get; set; }
         public string? quantityUnit { get; set; }
         
