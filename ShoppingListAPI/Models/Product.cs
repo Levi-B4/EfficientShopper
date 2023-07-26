a742bfb5332fa46e9f9af083553f9cfe232757fb
Levi-B4 <levi.butler4@gmail.com>
2023-07-26 18:31:34 -0400

    Added regex verification for product price.

--- ShoppingListAPI/Models/Product.cs
+++ ShoppingListAPI/Models/Product.cs
@@ -11,6 +11,7 @@ namespace ShoppingListAPI.Models
         public string name { get; set; }
         [Comment("Short description to clarify product if wanted")]
         public string? description { get; set; }
+        [RegularExpression("^(\\d{1, 3}(\\,\\d{3})*|(\\d+))(\\.\\d{2})?$")]
         public decimal? price { get; set; }
         public decimal quantity { get; set; }
         public string? quantityUnit { get; set; }
